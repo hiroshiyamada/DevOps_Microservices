b220569e96cdc372b0f302065706f0045f793052
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2022-09-28 12:54:45 +0900

    Add project4 codes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="cybermech/udacity-p4"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-p4-app\
+    --image=$dockerpath\
+    --port=80 --labels app=udacity-p4-app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-p4-app 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="cybermech/udacity-p4"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=cybermech
+docker tag udacity-p4 cybermech/udacity-p4:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push cybermech/udacity-p4:latest
